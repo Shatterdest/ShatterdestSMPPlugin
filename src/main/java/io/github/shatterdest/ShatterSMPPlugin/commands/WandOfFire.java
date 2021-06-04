ac9d56fbaaced3b4b17d78963654c08d728c7ca0
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2021-06-04 14:09:23 -0400

    changed withered bomb to wand of fire
    also lore is a thing xddddd

--- src/main/java/io/github/shatterdest/ShatterSMPPlugin/commands/WandOfFire.java
+++ src/main/java/io/github/shatterdest/ShatterSMPPlugin/commands/WandOfFire.java
@@ -1,19 +1,26 @@
 package io.github.shatterdest.ShatterSMPPlugin.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
-
+import org.bukkit.inventory.meta.ItemMeta;
 import io.github.shatterdest.ShatterSMPPlugin.Main;
+import io.github.shatterdest.ShatterSMPPlugin.misc.Glow;
+import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class WitheredBomb implements CommandExecutor {
+public class WandOfFire implements CommandExecutor {
     // This method is called, when somebody uses our command
-    public boolean onCommand(CommandSender sender, WitheredBomb cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, WandOfFire cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             // Create a new ItemStack (type: diamond)
@@ -29,19 +36,30 @@ public class WitheredBomb implements CommandExecutor {
     }
 
 	@Override
+	@EventHandler
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// TODO Auto-generated method stub
 	       if (sender instanceof Player) {
 	            Player player = (Player) sender;
 
-	            // Create a new ItemStack (type: brick)
-	            ItemStack tnt = new ItemStack(Material.TNT);
-
-	            // Set the amount of the ItemStack
-	            tnt.setAmount(1);
-
+                ItemStack blaze_rod = new ItemStack(Material.BLAZE_ROD, 1);
+	            
+	            ItemMeta meta = blaze_rod.getItemMeta();
+	            
+	            ArrayList<String> lore = new ArrayList();
+	            
+	            lore.add("A wand of fire");
+	            
+	            meta.setLore(lore);
+	            
+	            meta.setDisplayName(ChatColor.RED + "Wand of Fire");
+	            
+	            blaze_rod.setItemMeta(meta);
+	            
 	            // Give the player our items (comma-separated list of all ItemStack)
-	            player.getInventory().addItem(tnt);
+	            player.getInventory().addItem(blaze_rod);
+	            
+	            sender.sendMessage("You have been given Wand of Fire!");
 	            // Here we need to give items to our player
 		return true;
 	       }
